d2952fbdcf8dcaec571105ec984f037067f7dd12
Anatol Vasin <avasin@exadel.com>
2014-04-16 18:33:33 +0300

    mongoDB tests

--- Modules/cellModel/src/main/java/com/bio/cellscontrol/TestClass.java
+++ Modules/cellModel/src/main/java/com/bio/cellscontrol/TestClass.java
@@ -1,14 +1,41 @@
 package com.bio.cellscontrol;
 
-import org.springframework.util.SystemPropertyUtils;
+import com.mongodb.*;
 
-/**
- * Created by avasin-work on 16.04.14.
- */
+import java.net.UnknownHostException;
+
 public class TestClass {
 
-    public void start() {
+    private static final String collName = "names";
 
+    public static void main(String... args) {
+        try {
+            MongoClient mongo = new MongoClient("localhost", 27017);
+            for (String name : mongo.getDatabaseNames()) {
+                System.out.println(name);
+                final DB db = mongo.getDB(name);
+                if (name.equals("test")) {
+                    addObjectsToNames(db);
+                }
+                for (String collectionName : db.getCollectionNames()) {
+                    final DBCollection collection = db.getCollection(collectionName);
+                    System.out.println(collection.getName());
+                    final DBCursor cursor = collection.find();
+                    for (DBObject object : cursor) {
+                        System.err.println(object);
+                    }
+                }
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void addObjectsToNames(final DB db) {
+        DBCollection collection = db.getCollection(collName);
+        final DBObject obj = BasicDBObjectBuilder.start("name", "Alololsha").append("ages", 20).append("job", "programmer").get();
+        collection.insert(obj);
     }
 
 }
