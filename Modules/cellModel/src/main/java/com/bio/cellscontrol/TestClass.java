c317c9fd02e957bf06546296f28d30446dd212a0
Anatol Vasin <avasin@exadel.com>
2014-04-16 16:17:23 +0300

    python script added web config for application changed

---
+++ Modules/cellModel/src/main/java/com/bio/cellscontrol/TestClass.java
@@ -0,0 +1,14 @@
+package com.bio.cellscontrol;
+
+import org.springframework.util.SystemPropertyUtils;
+
+/**
+ * Created by avasin-work on 16.04.14.
+ */
+public class TestClass {
+
+    public void start() {
+
+    }
+
+}
